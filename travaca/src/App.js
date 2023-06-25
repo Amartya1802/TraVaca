a897ffa7e7a37839c90e9f640e43a4bab6c733fd
Amartya1802 <amartya21_ug@cse.nits.ac.in>
2023-06-26 02:52:47 +0530

    Initiated project, made required components

---
+++ travaca/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import Header from './Components/Header/Header';
+import LeftSection from './Components/LeftSection/LeftSection';
+import Map from './Components/Map/Map';
+import Carousel from './Carousel/Carousel/Carousel';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ travaca/src/Components/DetailsCard/DetailsCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailsCard = () => {
+    return (
+        <h1>Card</h1>
+    );
+}
+
+export default DetailsCard;
